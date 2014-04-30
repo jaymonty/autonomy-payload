568c516521541bbdc4f64527f156e24233be4c3b
Mike Clement <mrclemen@nps.edu>
2014-04-30 16:32:11 -0700

    NETWORK - completed Pose exchange node (aerial.py)
    Shares contents of a geometry_msgs/PoseStamped message type plus aircraft ID.
    Called ap_network_bridge/NetPoseStamped

--- ap_network_bridge/src/aerial.py
+++ ap_network_bridge/src/aerial.py
@@ -10,17 +10,14 @@
 # Import a bunch of libraries
 
 # Standard Python imports
-#import os
-#import sys
-#import signal
 import struct
 import socket
+import netifaces
 from optparse import OptionParser
 
 # General ROS imports
 import rospy
-from geometry_msgs.msg import PoseWithCovarianceStamped
-from tf.transformations import euler_from_quaternion
+from geometry_msgs.msg import PoseStamped, PoseWithCovarianceStamped
 
 # Import ROS messages specific to this bridge
 from ap_network_bridge import msg as netmsg
@@ -43,7 +40,8 @@ aircraft_id = None
 
 # UDP socket and info
 udp_sock = None
-udp_sock_ip = None
+udp_sock_local_ip = None
+udp_sock_bcast_ip = None
 udp_sock_port = None
 
 #-----------------------------------------------------------------------
@@ -62,70 +60,82 @@ def log_warn(msg):
 #-----------------------------------------------------------------------
 # UDP socket functions
 
-def sock_init(remote_ip, port):
-    global udp_sock, udp_sock_ip, udp_sock_port
-    udp_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    udp_sock.bind(('', port))
-    udp_sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-    udp_sock_ip = remote_ip
+def sock_init(device, port):
+    global udp_sock, udp_sock_local_ip, udp_sock_bcast_ip, udp_sock_port
+    
     udp_sock_port = port
+    try:
+        udp_sock_local_ip = netifaces.ifaddresses(device)[2][0]['addr']
+        udp_sock_bcast_ip = netifaces.ifaddresses(device)[2][0]['broadcast']
+    except Exception:
+        print "Could not get address info for device %s!"%device
+        return False
+    
+    udp_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    udp_sock.bind(('', udp_sock_port))
+    udp_sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    return True
 
 def sock_send(data):
-    global udp_sock, udp_sock_ip, udp_sock_port
-    udp_sock.sendto(data, (udp_sock_ip, udp_sock_port))
+    global udp_sock, udp_sock_bcast_ip, udp_sock_port
+    udp_sock.sendto(data, (udp_sock_bcast_ip, udp_sock_port))
 
 def sock_recv(max_size=1024):
     data = None
     try:
-        data, addr = udp_sock.recvfrom(max_size, socket.MSG_DONTWAIT)
-        # TODO check addr to see if it's me (and then loop?)
+        data, (ip, port) = udp_sock.recvfrom(max_size, socket.MSG_DONTWAIT)
+        #if ip == udp_sock_local_ip:
+        #    return True
     except Exception:
-        True
+        return None
     return data
 
 #-----------------------------------------------------------------------
 # Byte packing and unpacking
 
-PACKET_FORMAT = ">HHLLllllll"
+PACKET_FORMAT = ">HHLLLlllllll"
 '''
 Packet fields (in network byte order):
  - (16b) Aircraft ID (1..65535)
- - (16b) Message sequence number (1..65535)
+ - (16b) RESERVED
+ - (32b) Message sequence number (1..4Bil)
  - (32b) Seconds since epoch
  - (32b) Nanoseconds since last second
  - (32b) Latitude (deg) * 1e07 (truncated)
  - (32b) Longitude (deg) * 1e07 (truncated)
  - (32b) Altitude (m) * 1e03 (truncated)
- - (32b) Roll (rads) * 1e07 (truncated)
- - (32b) Pitch (rads) * 1e07 (truncated)
- - (32b) Yaw (rads) * 1e07 (truncated)
+ - (32b) Quat X (?) * 1e09 (truncated)
+ - (32b) Quat Y (?) * 1e09 (truncated)
+ - (32b) Quat Z (?) * 1e09 (truncated)
+ - (32b) Quat W (?) * 1e09 (truncated)
 '''
 
-def gps_pack(acid, seq, stamp, lat, lon, alt, rol, pit, yaw):
-    sec = int(stamp.secs)
-    nsec = int(stamp.nsecs)
-    lat = int(lat * 1e07)
-    lon = int(lon * 1e07)
-    alt = int(alt * 1e03)
-    rol = int(rol * 1e07)
-    pit = int(pit * 1e07)
-    yaw = int(yaw * 1e07)
-    return struct.pack(PACKET_FORMAT, acid, seq, sec, nsec, 
-                       lat, lon, alt, rol, pit, yaw)
+def pose_pack(acid, seq, secs, nsecs, lat, lon, alt, q_x, q_y, q_z, q_w):
+    return struct.pack(PACKET_FORMAT, 
+                       acid, 
+                       0x0000,
+                       seq,
+                       secs,
+                       nsecs,
+                       int(lat * 1e07),
+                       int(lon * 1e07),
+                       int(alt * 1e03),
+                       int(q_x * 1e09),
+                       int(q_y * 1e09),
+                       int(q_z * 1e09),
+                       int(q_w * 1e09))
 
-def gps_unpack(data):
-    acid, seq, sec, nsec, lat, lon, alt, rol, pit, yaw \
+def pose_unpack(data):
+    acid, unused1, seq, secs, nsecs, lat, lon, alt, q_x, q_y, q_z, q_w \
         = struct.unpack(PACKET_FORMAT, data)
-    stamp = rospy.Time()
-    stamp.secs = sec
-    stamp.nsecs = nsec
     lat = float(lat) / 1e07
     lon = float(lon) / 1e07
     alt = float(alt) / 1e03
-    rol = float(rol) / 1e07
-    pit = float(pit) / 1e07
-    yaw = float(yaw) / 1e07
-    return acid, seq, stamp, lat, lon, alt, rol, pit, yaw
+    q_x = float(q_x) / 1e09
+    q_y = float(q_y) / 1e09
+    q_z = float(q_z) / 1e09
+    q_w = float(q_w) / 1e09
+    return acid, seq, secs, nsecs, lat, lon, alt, q_x, q_y, q_z, q_w
 
 #-----------------------------------------------------------------------
 # Subscribers (ROS -> Network)
@@ -134,19 +144,17 @@ def sub_pose(msg):
     global aircraft_id
     
     # Package contents into a datagram
-    rol, pit, yaw = euler_from_quaternion([msg.pose.pose.orientation.x,
-                                           msg.pose.pose.orientation.y,
-                                           msg.pose.pose.orientation.z,
-                                           msg.pose.pose.orientation.w],
-                                           'sxyz')
-    dgram = gps_pack(aircraft_id, msg.header.seq, msg.header.stamp,
-                     msg.pose.pose.position.x, 
-                     msg.pose.pose.position.y, 
-                     msg.pose.pose.position.z, 
-                     rol, pit, yaw)
-    log_dbug("(aerial) X: %f Y: %f Z: %f R: %f P: %f Y: %f" % \
-             (msg.pose.pose.position.x, msg.pose.pose.position.y,
-              msg.pose.pose.position.z, rol, pit, yaw))
+    dgram = pose_pack(aircraft_id, 
+                      msg.header.seq, 
+                      msg.header.stamp.secs,
+                      msg.header.stamp.nsecs,
+                      msg.pose.pose.position.x, 
+                      msg.pose.pose.position.y, 
+                      msg.pose.pose.position.z, 
+                      msg.pose.pose.orientation.x,
+                      msg.pose.pose.orientation.y,
+                      msg.pose.pose.orientation.z,
+                      msg.pose.pose.orientation.w)
     # Send out via network
     sock_send(dgram)
 
@@ -158,8 +166,8 @@ if __name__ == '__main__':
     parser = OptionParser("aerial.py [options]")
     parser.add_option("--id", dest="acid", type='int',
                       help="Aircraft ID", default=1)
-    parser.add_option("--ip", dest="ip", 
-                      help="IP address to send to", default="192.168.2.1")
+    parser.add_option("--device", dest="device", 
+                      help="Network device to use", default="wlan0")
     parser.add_option("--port", dest="port", type='int',
                       help="UDP port", default=5554)
     parser.add_option("--sub", dest="sub",
@@ -167,19 +175,19 @@ if __name__ == '__main__':
                       default="estimator/odom")
     parser.add_option("--pub", dest="pub",
                       help="ROS topic to publish to", 
-                      default="net/pose")
+                      default="network/pose")
     (opts, args) = parser.parse_args()
     
     # Initialize ROS and socket
     rospy.init_node('net_aerial')
-    sock_init(opts.ip, opts.port)
+    sock_init(opts.device, opts.port)
     aircraft_id = opts.acid
     
     # Set up subscriber (ROS -> network)
     rospy.Subscriber(opts.sub, PoseWithCovarianceStamped, sub_pose)
     
     # Set up publisher (network -> ROS)
-    pub = rospy.Publisher(opts.pub, netmsg.shared_lla)
+    pub = rospy.Publisher(opts.pub, netmsg.NetPoseStamped)
     
     # Loop , checking for incoming datagrams and sleeping
     r = rospy.Rate(10)
@@ -187,17 +195,27 @@ if __name__ == '__main__':
     while not rospy.is_shutdown():
         # Check for datagrams
         while (True):
-            break # TODO fix self-listening behavior in sock_recv()
-
             dgram = sock_recv()
-            if not dgram:
+            if dgram == None:
                 break
+            elif dgram == True:
+                continue
             
             # Convert into ROS message and publish
-            acid, seq, stamp, lat, lon, alt, rol, pit, yaw \
-                = gps_unpack(dgram)
-            msg = netmsg.shared_lla(acid, seq, stamp, 
-                                    lat, lon, alt, rol, pit, yaw)
+            acid, seq, secs, nsecs, lat, lon, alt, q_x, q_y, q_z, q_w \
+                = pose_unpack(dgram)
+            msg = netmsg.NetPoseStamped()
+            msg.sender_id = acid 
+            msg.pose.header.stamp.secs = secs
+            msg.pose.header.stamp.nsecs = nsecs
+            msg.pose.header.seq = seq 
+            msg.pose.pose.position.x = lat 
+            msg.pose.pose.position.y = lon 
+            msg.pose.pose.position.z = alt 
+            msg.pose.pose.orientation.x = q_x
+            msg.pose.pose.orientation.y = q_y
+            msg.pose.pose.orientation.z = q_z
+            msg.pose.pose.orientation.w = q_w
             pub.publish(msg)
         
         # No more messages, so sleep briefly
