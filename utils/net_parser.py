b0fd7dcad7a70020bab5bde45619838173653203
Mike Clement <mrclemen@nps.edu>
2014-06-17 13:21:22 -0700

    UTILS: Fixed net_parser.py to use last commit
    Changed option from --ip to --device and have hard-coded
    case for loopback device.

--- utils/net_parser.py
+++ utils/net_parser.py
@@ -22,16 +22,23 @@ from acs_socket import Socket
 if __name__ == '__main__':
     # Grok args
     parser = OptionParser("pose_parser.py [options]")
-    parser.add_option("--ip", dest="ip", 
-                      help="Network address to listen on", default='')
+    parser.add_option("--device", dest="device", 
+                      help="Network device to listen on", default='')
     parser.add_option("--port", dest="port", type="int",
                       help="Network port to listen on", default=5554)
     parser.add_option("--repeat", dest="repeat",
                       action="store_true", default=False)
     (opts, args) = parser.parse_args()
     
+    # NOTE: This is a hack to work with SITL
+    my_ip = None
+    bcast_ip = None
+    if opts.device == 'lo':
+        my_ip = '127.0.1.1'
+        bcast_ip = '127.0.0.1'
+    
     try:
-        sock = Socket(0xff, opts.port, None, opts.ip, '127.0.0.1')
+        sock = Socket(0xff, opts.port, opts.device, my_ip, bcast_ip)
     except Exception:
         print "Sorry, couldn't start up the listening socket"
         sys.exit(1)
