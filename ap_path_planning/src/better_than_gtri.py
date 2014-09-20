0242981b88fba519ec52fbf4de34e0eef63594bd
Jorgensen, Stefan T <stjorgen@nps.edu>
2014-09-19 22:19:31 -0700

    PATH_PLANNING: Implemented improved follow_me
    As well as dubins based follow_me. Still single leader.

---
+++ ap_path_planning/src/better_than_gtri.py
@@ -0,0 +1,208 @@
+#!/usr/bin/env python
+
+# Rudimentary follow-me code
+# Listens to incoming network poses, downsamples, 
+#   does some math, and sends coords to local autopilot
+# Improved version written by Stefan Jorgensen, August 2014
+
+import sys
+import rospy
+from argparse import ArgumentParser
+from autopilot_bridge.msg import LLA
+from ap_network_bridge.msg import NetOdometry
+
+from nav_msgs.msg import Odometry
+import math
+from ap_lib import gps_utils
+
+
+# These are set by command line arguments
+ # Leader ID
+apid = None
+ #base altitude
+BASE_ALT= None
+ #altitude separation
+ALT_SEP = None
+ # Target following distance from leader
+Rfollow = -10 #None #units meters
+ # Overshoot distance for avoiding loiter 
+Rovershoot = 60 #units meters
+
+# These are set by incoming ROS messages
+tstamp = None
+lat = None
+lon = None
+alt = None
+self_lat = None
+self_lon = None
+self_alt = None
+
+# Compute target waypoints with overshoot
+def compute_follow_wp(lat2, lon2, lat1, lon1, self_lat, self_lon):
+    global Rfollow, Rovershoot
+    radius_of_earth = 6378100.0 #in meters
+    radius_at_lat = radius_of_earth*math.cos((math.radians(lat1)+math.radians(lat2))/2)
+
+  # Check for valid input
+    if self_lat is None or self_lon is None:
+        print "Missing self data"
+        return None
+    if lat2 is None or lon2 is None:
+        print "Missing new leader data"
+        return None
+    if lat1 is None or lon1 is None:
+        print "Missing prior leader data"
+        return None
+
+  # Project line back from leader:
+    leader_bearing = gps_utils.gps_bearing(lat1,lon1,lat2,lon2)
+    #Here we make an arclength assumption.
+    a_lat = math.radians(lat2) - (Rfollow*math.cos(math.radians(leader_bearing)))/radius_of_earth 
+    a_lon = math.radians(lon2) - (Rfollow*math.sin(math.radians(leader_bearing)))/radius_at_lat 
+
+  # Project line forward:
+    ab_bearing = gps_utils.gps_bearing(self_lat, self_lon, math.degrees(a_lat),math.degrees(a_lon))
+    b_lat = math.degrees(a_lat + (Rovershoot*math.cos(math.radians(ab_bearing)))/radius_of_earth)
+    b_lon = math.degrees(a_lon + (Rovershoot*math.sin(math.radians(ab_bearing)))/radius_at_lat)
+
+    return (b_lat, b_lon)
+
+# Handle incoming network pose messages
+def pose_callback(data):
+    global apid, tstamp, lat, lon, alt
+    try:
+        # Don't use poses from other than target aircraft
+        if apid != data.sender_id:
+            #print "Ignoring pose from wrong ID"
+            return
+
+        # Don't use poses not newer than last-heard
+        t = float(data.odom.header.stamp.secs) + \
+            float(data.odom.header.stamp.nsecs) / float(1e9)
+        if tstamp is not None and t <= tstamp:
+            print "Ignoring pose older than one received"
+            return
+
+        # Update last-heard state
+        tstamp = t
+        lat = data.odom.pose.pose.position.x
+        lon = data.odom.pose.pose.position.y
+        # DON'T USE UNLESS DOING ALTITUDE MATCHING
+        alt = data.odom.pose.pose.position.z
+    except Exception as ex:
+        # If there's an error, null out data
+        print "Net Pose Callback error: " + ex.args[0]
+        tstamp = None  # Null so we don't get stuck in the future
+        lat = None
+        lon = None
+        # DON'T USE UNLESS DOING ALTITUDE MATCHING
+        alt = None
+
+
+# Handle incoming gps odom messages
+def odom_callback(data):
+    global self_lat, self_lon, self_alt
+    try:
+        self_lat = data.pose.pose.position.x
+        self_lon = data.pose.pose.position.y
+        self_alt = data.pose.pose.position.z
+    except Exception as ex:
+        # If there's an error, null out data
+        print "Odom Callback error: " + ex.args[0]
+        self_lat = None
+        self_lon = None
+        self_alt = None
+
+if __name__ == '__main__':
+    # Parse command line arguments
+    parser = ArgumentParser("rosrun ap_path_planning follow_me.py")
+    parser.add_argument("aircraft", help="ID of aircraft to follow")
+    #parser.add_argument("Rfollow", help="following distance")
+    parser.add_argument("--use-base-alt", dest="BASE_ALT", default=None)
+    parser.add_argument("--use-alt-sep", dest="ALT_SEP",default=None)
+    args = parser.parse_args(args=rospy.myargv(argv=sys.argv)[1:])
+    if args.BASE_ALT is not None:
+        BASE_ALT = float(args.BASE_ALT)
+        print "Using constant altitude %f" % (BASE_ALT)
+    elif args.ALT_SEP is not None:
+        ALT_SEP = float(args.ALT_SEP)
+        print "Using altitude difference %f" % (ALT_SEP)
+        if BASE_ALT is not None:
+            print "Please use only --use-base-alt or --use-alt-sep options"
+            sys.exit(1)
+    else:
+        print "Please supply either base altitude or separation altitude (--use-base-alt BASE_ALT or --use-alt-sep ALT_SEP"
+        sys.exit(1)
+    
+    apid = int(args.aircraft)
+    # Rfollow = float(args.Rfollow) 
+    # Initialize ROS node
+    rospy.init_node("follow_me")
+
+    # Listen to network pose messages
+    sub_pose = rospy.Subscriber("/network/recv_pose",
+                                NetOdometry,
+                                pose_callback)
+
+    # Listen to self pose messages
+    sub_odom = rospy.Subscriber("/autopilot/gps_odom",
+                                      Odometry,
+                                      odom_callback)
+
+    # Publish to autopilot's WP navigation
+#    pub_pl_wp = rospy.Publisher("/autopilot/payload_waypoint",
+#                                LLA)
+    pub_pl_wp = rospy.Publisher("/autopilot/guided_goto",LLA)
+
+    print "\nFollowing aircraft %u ...\n" % (apid)
+
+    #Containers for storing previous lat/lon data for leader
+    last_lat = None
+    last_lon = None
+
+    # Loop at 1 Hz so we don't overwhelm the autopilot
+    r = rospy.Rate(1.0)
+    while not rospy.is_shutdown():
+        target_wp = None 
+        if lat is not None and lon is not None:
+            if last_lat is None or last_lon is None:
+                # Need to initialize
+                last_lat = lat
+                last_lon = lon
+            #elif self_lat is not None or self_lon is not None:
+            else:
+                target_wp = compute_follow_wp(lat, lon, last_lat, last_lon, self_lat, self_lon)
+        # Only send if there is valid data to use
+            if target_wp is not None and \
+              tstamp is not None and alt is not None and self_alt is not None:
+                target_lat, target_lon = target_wp
+                # Build and send message
+                lla = LLA()
+                lla.lat = target_lat
+                lla.lon = target_lon
+                lla.alt = None
+            # DON'T USE UNLESS DOING ALTITUDE MATCHING
+                if BASE_ALT is not None:
+                    lla.alt = BASE_ALT
+                elif ALT_SEP is not None:
+                    lla.alt = alt + ALT_SEP
+                if lla.alt is not None: #verify valid altitude data
+                    pub_pl_wp.publish(lla)
+                    print "Sent to (%0.06f, %0.06f, %0.03f (leader alt: %0.03f))" % (lat, lon, lla.alt, alt)
+
+            # Push old data to old containers
+            last_lat = lat
+            last_lon = lon
+
+            # Null out old data so we don't resend the same point
+            lat = None
+            lon = None
+            self_lat = None
+            self_lon = None
+            # DON'T USE UNLESS DOING ALTITUDE MATCHING
+            alt = None
+
+        # Don't check again for the rest of the second
+        r.sleep()
+
+
---
+++ ap_path_planning/src/formation.py
@@ -0,0 +1,246 @@
+#!/usr/bin/env python
+
+#-----------------------------------------------
+# Formation
+# Stefan Jorgensen
+# 
+#-----------------------------------------------
+
+import sys
+import rospy
+#Append libraries directory to path:
+from ap_lib.dubins import *
+# Keeps track of agents in the networ
+from ap_perception.swarm_tracker import *
+from ap_lib import gps_utils
+from argparse import ArgumentParser
+from autopilot_bridge.msg import LLA
+import math
+
+STATE_TYPE =0
+# These are globals set by command line arguments
+ # Flock leader:
+flockLeaderID = None
+ownID = None
+ # Base altitude
+BASE_ALT = None
+ # Altitude separation
+ALT_SEP = None
+ #Following distance from leader
+Rfollow = -10 #units meters
+Rovershoot = 60 #units meters
+leaderUpdateCount =10 #how long before updating leader
+
+#Home point, used for computing relative metric distance (dubins paths)
+HOME_LAT = 35.7167
+HOME_LON = -120.764508
+dubins_rad = 20.0
+dubins_step = 2.0
+
+#meters_to_lla(home_lat, home_lon, target_mx, target_my, target_malt)
+#Converts meters from home to lla values. 
+#TODO: Incorporate into general GPS distance library
+# @home_lat, home_lon: Home latitude, longidtude 
+# @target_mx, target_my: meters from Home in x, y directions (+x is E, +y is N)
+# @target_malt: altitude, passed straight through
+def meters_to_lla(home_lat, home_lon, target_mx, target_my, target_malt):
+    #Assume that the earth is perfectly round
+    radius_of_earth = 6378100.0
+    #Radius changes as you move north
+    radius_at_lat = radius_of_earth*math.cos(math.radians(home_lat))
+
+    lat = home_lat + math.degrees(target_my/radius_of_earth)
+    lon = home_lon + math.degrees(target_mx/radius_at_lat)
+    return (lat,lon,target_malt)
+
+
+#Compute target Waypoints with overshoot
+def compute_follow_wp(leaderID, tracker):
+    global Rfollow, Rovershoot,STATE_TYPE
+
+    #Test whether leaderID is in the swarm:
+    if leaderID not in tracker.swarm:
+        print "Can't see leader %u yet"%leaderID
+        return None
+
+    #Check contents
+    leader = tracker.swarm[leaderID]
+    l_lla = leader.getPosition(STATE_TYPE) #stores (lat, lon, alt)
+    myself = tracker.swarm[ownID]    
+    m_lla = myself.getPosition(STATE_TYPE)
+    if leader is None or myself is None: 
+        print "Missing data"
+        return None
+
+    #load current/past leader lat/lon into containers
+    radius_of_earth = 6378100.0
+    radius_at_lat = radius_of_earth*math.cos((math.radians(l_lla[0] + m_lla[0]))/2)
+
+    #Compute the forward projection based on leader heading
+    rpy = leader.getEulerAngles(STATE_TYPE)
+    leader_angle = rpy[2]
+
+    #Project a line back (Point A)
+    a_lat = math.radians(l_lla[0]) - (Rfollow*math.cos(leader_angle))/radius_of_earth
+    a_lon = math.radians(l_lla[1])  - (Rfollow*math.sin(leader_angle))/radius_at_lat
+    
+    #Project a line forward (Point B)
+    a_angle = math.radians(gps_utils.gps_bearing(m_lla[0], m_lla[1], a_lat, a_lon))
+    b_lat = math.degrees(a_lat + (Rovershoot*math.cos(a_angle))/radius_of_earth)
+    b_lon = math.degrees(a_lon + (Rovershoot*math.sin(a_angle))/radius_at_lat)
+
+    return (b_lat, b_lon)
+
+
+
+#Perform algorithm for V formation
+def find_local_leader(flockLeaderID, tracker):
+    global STATE_TYPE
+    # Find leader object
+    if flockLeaderID not in tracker.swarm or ownID not in tracker.swarm:
+       print "no data"
+       return None
+    
+    leader = tracker.swarm[flockLeaderID]
+    myself = tracker.swarm[ownID]
+
+    #Here we convert yaw into coordinates which play nicely with the dubins generator (might not be necessary?)
+    #Get self configuration for purposes of dubins path lengths
+    m_lat,m_lon,m_alt = myself.getPosition(STATE_TYPE)
+    m_roll,m_pitch,m_yaw = myself.getEulerAngles(STATE_TYPE)
+    self_R = gps_utils.gps_distance(HOME_LAT, HOME_LON, m_lat, m_lon)
+    self_bearing = math.radians(gps_utils.gps_bearing(HOME_LAT, HOME_LON, m_lat, m_lon))
+    self_config = (self_R*math.cos(self_bearing),self_R*math.sin(self_bearing),math.pi/2-m_yaw)
+
+    #Get leader configuration for purposes of dubins path lengths
+    l_lat,l_lon,l_alt = leader.getPosition(STATE_TYPE)
+    l_roll,l_pitch,l_yaw = leader.getEulerAngles(STATE_TYPE)
+    leader_R = gps_utils.gps_distance(HOME_LAT, HOME_LON, l_lat, l_lon)
+    leader_bearing = math.radians(gps_utils.gps_bearing(HOME_LAT, HOME_LON, l_lat, l_lon))
+    leader_config = (leader_R*math.cos(leader_bearing), leader_R*math.sin(leader_bearing), math.pi/2-l_yaw)
+
+    #Get distance to go straight to leader
+    MAX_DISTANCE = dubins.path_length(self_config, leader_config, dubins_rad)
+
+    #Initialize to flockLeader
+    local_leader = flockLeaderID
+
+    #Look for agents closer to leader than myself; 
+    #  choose to follow the nearest of these
+    MAX_NEARNESS = 100 #The furthest leader you are willing to follow (units vary depending on weighting. See below)
+
+    closest = MAX_NEARNESS
+
+    #Perform exhaustive search (will not scale)
+    for agentID in tracker.swarm:
+        #consider agents not self or leader:
+        if agentID is not flockLeaderID and agentID is not ownID:
+
+            agent = tracker.swarm[agentID]
+
+            #Form configuration for agent
+            a_lat,a_lon,a_alt = agent.getPosition(STATE_TYPE)
+            a_roll,a_pitch,a_yaw = agent.getEulerAngles(STATE_TYPE)
+            agent_R = gps_utils.gps_distance(HOME_LAT, HOME_LON, a_lat, a_lon)
+            agent_bearing = math.radians(gps_utils.gps_bearing(HOME_LAT, HOME_LON, a_lat, a_lon)) 
+            agent_config = (agent_R*math.cos(agent_bearing), agent_R*math.sin(agent_bearing), math.pi/2-a_yaw)
+            #TODO: revisit this logic.
+
+            #find minimum distance path from agent to leader
+            distance_al = dubins.path_length(agent_config, leader_config, dubins_rad)
+
+            #Don't consider if further from leader than you are.
+            if distance_al > MAX_DISTANCE:
+                continue
+
+            #find minimum distance path from self to agent 
+            distance_sa = dubins.path_length(self_config, agent_config, dubins_rad)
+            #don't consider if further from leader
+            if distance_sa > MAX_DISTANCE:
+                continue 
+            elif distance_sa < closest:
+                closest = distance_sa
+                local_leader = agentID
+
+    if local_leader not in tracker.swarm:
+        print "Found a bug! I thought %u was in the swarm:"%local_leader
+        print swarm
+        return flockLeaderID
+    return local_leader
+
+if __name__ == '__main__':
+    #Parse arguments
+    parser = ArgumentParser("rosrun ap_path_planning follow_us.py")
+    parser.add_argument("leaderID",help="ID of flock leader")
+    parser.add_argument("selfID",help="ID of self")
+    parser.add_argument("--use-base-alt",help="altitude to maintain during flight",dest="BASE_ALT",default=None)
+    parser.add_argument("--use-alt-sep",help="altitude separation to maintain from leader",dest="ALT_SEP",default=None)
+    args = parser.parse_args(args=rospy.myargv(argv=sys.argv)[1:])
+    if args.BASE_ALT is not None:
+        BASE_ALT = float(args.BASE_ALT)
+        print "Using constant altitude %f" % (BASE_ALT)
+    elif args.ALT_SEP is not None:
+        ALT_SEP = float(args.ALT_SEP)
+        if BASE_ALT is not None:
+            print "Please use either the --use-base-alt or --use-alt-sep option"
+            sys.exit(1)
+        print "Using altitude separation %f" % (ALT_SEP)
+    else:
+        print "Please supply either base altitude or separation altitude (--use-base-alt (alt in meters) or --use-alt-sep (alt_sep in meters)"
+        sys.exit(1)
+    flockLeaderID = int(args.leaderID)
+    ownID = int(args.selfID)
+
+    #Initialize ROS nodes
+    rospy.init_node("follow_us")
+    #pub_pl_wp = rospy.Publisher("/autopilot/guided_goto",LLA)
+    pub_pl_wp = rospy.Publisher("/autopilot/guided_goto",LLA)
+    print "\n Agent %u flocking around aircraft %u....\n" % (ownID,flockLeaderID)
+    tracker = SwarmTrackerSubscriber()
+    leaderUpdateCounter=0
+    leader_ID = flockLeaderID
+    r = rospy.Rate(1.0)
+    lastLeader = 0;
+    while not rospy.is_shutdown():
+       
+        target_wp =  None
+        #check for valid data (should be more thorough)
+        if tracker is not None and tracker.swarm.get(ownID,None) is not None \
+          and tracker.swarm.get(flockLeaderID,None) is not None:
+            if leaderUpdateCounter == leaderUpdateCount:
+                print "Re-evaluating leader"
+                leaderUpdateCounter = 0
+                #leader_ID = find_local_leader(flockLeaderID, tracker)
+            else:
+                leaderUpdateCounter+=1
+            leader_lat=None
+            leader_lon=None
+            leader_alt=None
+            if leader_ID is not None:
+                if leader_ID is not lastLeader:
+                    print "Now following agent %u..." % leader_ID
+                    lastLeader = leader_ID
+#                target_wp = compute_follow_wp(leader_ID, tracker)
+                if leader_ID in tracker.swarm:
+                    leader_lat,leader_lon,leader_alt = tracker.swarm[leader_ID].getPosition(STATE_TYPE)
+                    target_wp = (leader_lat,leader_lon)
+                else:
+                    print "Targeting leader %u, but can't find him!"%leader_ID
+                    print tracker.swarm
+            if target_wp is not None:
+                target_lat, target_lon = target_wp
+                #Build and send message
+                lla = LLA()
+                lla.lat, lla.lon = target_wp
+                lla.alt = None
+                if BASE_ALT is not None:
+                    lla.alt = BASE_ALT
+                elif ALT_SEP is not None:
+                    print "Warning: this altitude implementation is not complete (does not guarantee airspace separation when leaders switch)"
+                    lla.alt = leader_alt + ALT_SEP
+                if lla.alt is not None:
+                    pub_pl_wp.publish(lla)
+                    print "Sent to (%0.06f, %0.06f, %0.03f)" % (lla.lat, lla.lon, lla.alt)
+                else:
+                    print "Invalid altitude command. No waypoint sent"
+        r.sleep()
