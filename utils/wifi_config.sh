ca8f44545015cb68dca8df6846b9fdb50a923559
Mike Clement <mrclemen@nps.edu>
2014-12-13 10:32:46 -0800

    UTILS: Minor improvements to wifi_config.sh

--- utils/wifi_config.sh
+++ utils/wifi_config.sh
@@ -2,9 +2,10 @@
 
 if [ -z $1 ] || [ -z $2 ]; then
   echo "usage: $0 DEVICE ID-NUMBER"
+  exit 1
 fi
 
-ifconfig $1 down
-iwconfig $1 mode ad-hoc essid zephyr channel 6 ap 00:11:22:33:44:55
-ifconfig $1 inet 192.168.2.$2/24 up
+sudo ifconfig $1 down
+sudo iwconfig $1 mode ad-hoc essid zephyr channel 6 ap 00:11:22:33:44:55
+sudo ifconfig $1 inet 192.168.2.$2/24 up
 
