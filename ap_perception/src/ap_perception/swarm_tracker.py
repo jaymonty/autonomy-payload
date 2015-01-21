fa73042af28f62acd494654346dc00526557c429
Duane Davis <dtdavi1@nps.edu>
2015-01-21 05:40:49 -0800

    PERCEPTION:  ROS topic queue_size
    Incorporated the ROS topic queue_size parameter into swarm_tracker.
    The file contains classes that inherit from the Nodeable class, so
    the parameter is added to the createPublisher method invocations.

--- ap_perception/src/ap_perception/swarm_tracker.py
+++ ap_perception/src/ap_perception/swarm_tracker.py
@@ -357,9 +357,9 @@ class SwarmTracker(Nodeable):
     # @param params: list of parameters (none required for this method)
     def publisherSetup(self, params=[]):
         self._swarmPublisher = \
-            self.createPublisher("swarm_state", SwarmStateStamped)
+            self.createPublisher("swarm_state", SwarmStateStamped, 1)
         self._subSwarmPublisher = \
-            self.createPublisher("subswarm_state", SwarmStateStamped)
+            self.createPublisher("subswarm_state", SwarmStateStamped, 1)
 
 
     # Executes one iteration of the timed loop for the SwarmTracker object
