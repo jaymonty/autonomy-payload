0a093aa073057d66fa130629af7b6fcf70f6785e
Mike Clement <mrclemen@nps.edu>
2015-02-24 20:30:40 -0800

    UTILS: Added txpower option to wifi_config
    Now defaults to setting wifi power to "10" (good for Alfa cards,
    unsure for others). Also allows -T <power> option.

--- utils/wifi_config.sh
+++ utils/wifi_config.sh
@@ -6,15 +6,17 @@ cat <<EOF
 Usage: $0 [options] wireless-device id-number
 Options:
     -R DEVICE       Set up as router, with DEVICE facing the world
+    -T POWER        Set txpower (default=10)
 EOF
 }
 
 # option defaults
 ROUTER_USE=0
 ROUTER_DEV=""
+TXPOWER=10
 
 #parse options
-while getopts ":R:h" opt; do
+while getopts ":R:T:h" opt; do
     case $opt in
         R)
             if [ -z $OPTARG ]; then
@@ -23,6 +25,13 @@ while getopts ":R:h" opt; do
             fi
             ROUTER_USE=1
             ROUTER_DEV=$OPTARG
+            ;;
+        T)
+            if [ -z $OPTARG ]; then
+                usage
+                exit 1
+            fi
+            TXPOWER=$OPTARG
             ;;
         h)
             usage
@@ -44,6 +53,7 @@ if [ $ROUTER_USE != 0 ]; then
 fi
 
 sudo ifconfig $1 down
-sudo iwconfig $1 mode ad-hoc essid zephyr channel 6 ap 00:11:22:33:44:55
+sudo iwconfig $1 mode ad-hoc essid zephyr channel 6 \
+  ap 00:11:22:33:44:55 txpower $TXPOWER
 sudo ifconfig $1 inet 192.168.2.$2/24 up
 
