5052ee124bffc30a70aa99dcae093618ce455505
Mike Clement <mrclemen@nps.edu>
2015-10-19 08:50:58 -0700

    SAFETY: New safety monitor, initial commit
    This safety.py replaces the previous safety.py, with additional
    checks and more robust coding. It confirms that:
     - "Core" nodes are alive (configurable in code)
     - AP is producing pose messages
     - Has/not been commanded to disable (as previously implemented)
     - Ground heartbeat is active (currently disabled)
    
    It also provides a new topic, safety/healthy, that is a latched
    Boolean, set whenever the health state changes.
    
    master.launch is updated to do correct topic remapping.

--- ap_safety_monitor/src/safety.py
+++ ap_safety_monitor/src/safety.py
@@ -14,49 +14,172 @@
 
 # General ROS imports
 import rospy
+from rosgraph import masterapi
 
 # Import ROS message and service types
+from std_msgs import msg as stdmsg
 from autopilot_bridge import msg as apmsg
+from ap_msgs import msg as ap_msgs
 from ap_srvs import srv as ap_srvs
 
 #-----------------------------------------------------------------------
-# Parameters
+# SafetyMonitor class
 
-# Base name for node topics and services
-ROS_BASENAME = 'safety'
+class SafetyMonitor(object):
 
-#-----------------------------------------------------------------------
-# Ugly global variables
+    def __init__(self):
+        # Internal variables
+        self._health_state = None       # Last-known health state
+        self._health_counter = 0        # Counter of published heartbeats
+        self._set_state = True          # Commanded health state
+        self._hb_last_msg = None        # Last ground heartbeat
+        self._hb_last_time = 0.0
+        self._hb_interval = 10.0
+        self._ap_last_msg = None        # Last autopilot pose
+        self._ap_last_time = 0.0
+        self._ap_interval = 3.0
+        self._mapi = masterapi.Master('safety') # ROS Master API
+        self._mapi_nodes = ['autopilot',
+                            'network',
+                            'swarm_tracker',
+                            'task_runner']
+        self._mapi_namespace = rospy.get_namespace()
+        self._mapi_last_result = True
+        self._mapi_last_time = 0.0
+        self._mapi_interval = 5.0
 
-# Overall, is the system healthy and should we send heartbeats?
-all_is_well = True
+        # ROS objects
+        bname = rospy.get_name()
+        self._pub_heartbeat = rospy.Publisher(bname+"/heartbeat",
+                                              apmsg.Heartbeat,
+                                              queue_size=1)
+        self._pub_healthy = rospy.Publisher(bname+"/healthy",
+                                            stdmsg.Bool,
+                                            queue_size=1,
+                                            latch=True)
+        self._sub_heartbeat = rospy.Subscriber(bname+"/gnd_heartbeat",
+                                               apmsg.Heartbeat,
+                                               self._sub_ground_heartbeat)
+        self._sub_pose = rospy.Subscriber(bname+"/ap_pose",
+                                          apmsg.Geodometry,
+                                          self._sub_autopilot_pose)
+        self._srv_state = rospy.Service(bname+"/set_health_state",
+                                        ap_srvs.SetBoolean,
+                                        self._srv_set_state)
 
-#-----------------------------------------------------------------------
-# ROS Subscriber callbacks
+    #-------------------------------------------------------------------
+    # Main loop
 
+    def run(self):
+        ''' Main loop '''
+        COUNTER_MAX = 2**32
+        hb = apmsg.Heartbeat()
+        healthy = stdmsg.Bool()
+        r = rospy.Rate(1.0)
 
+        while not rospy.is_shutdown():
+            # Check all safety conditions
+            ok = self._is_ok()
 
-#-----------------------------------------------------------------------
-# ROS services
+            # Publish a heartbeat, only if all is well
+            if ok:
+                self._health_counter = (self._health_counter + 1) % COUNTER_MAX
+                hb.counter = self._health_counter
+                self._pub_heartbeat.publish(hb)
 
-# Enable or disable heartbeat, depending on data.enable
-def svc_set_health(data):
-    global all_is_well
-    if data.enable == 0:
-        all_is_well = False
-    else:
-        all_is_well = True
-    return 1
+            # If health has changed, publish to latched topic
+            if ok != self._health_state:
+                self._health_state = ok
+                healthy.data = ok
+                self._pub_healthy.publish(healthy)
 
-#-----------------------------------------------------------------------
-# Manage heartbeat counter
+            # Sleep so ROS subscribers and services can run
+            r.sleep()
 
-hb_counter = 0
+    def _is_ok(self):
+        ''' Return boolean indicating if system is "ok" '''
+        ok = True
+        for check in [self._chk_set_state,
+                      self._chk_nodes_alive,
+                      #self._chk_ground_heartbeat,  # TODO No HB in SITL yet
+                      self._chk_autopilot_pose]:
+            try:
+                if not check():
+                    rospy.logdebug("SAFETY: check %s failed" % \
+                                   check.__name__)
+                    ok = False
+            except Exception as ex:
+                rospy.logwarn("SAFETY: check %s error: %s" % \
+                              (check.__name__, str(ex)))
+                ok = False
+        return ok
 
-def next_counter():
-    global hb_counter
-    hb_counter += 1
-    return hb_counter
+    #-------------------------------------------------------------------
+    # ROS subscriptions and services
+
+    def _sub_ground_heartbeat(self, msg):
+        ''' Callback for received ground heartbeat '''
+        self._hb_last_msg = msg
+        self._hb_last_time = rospy.get_time()
+
+    def _sub_autopilot_pose(self, msg):
+        ''' Callback for autopilot pose message '''
+        self._ap_last_msg = msg
+        self._ap_last_time = rospy.get_time()
+
+    def _srv_set_state(req):
+        ''' Callback for setting explicit health state '''
+        self._set_state = bool(req.enable)
+        return 1
+
+    #-------------------------------------------------------------------
+    # Checking methods
+
+    def _chk_set_state(self):
+        ''' Check whether health state was explicitly commanded '''
+        return self._set_state
+
+    def _chk_nodes_alive(self):
+        ''' Check if critical ROS nodes are alive '''
+        t = rospy.get_time()
+        if t < self._mapi_last_time + self._mapi_interval:
+            return self._mapi_last_result
+
+        self._mapi_last_result = True
+        self._mapi_last_time = t
+
+        for node in self._mapi_nodes:
+            name = self._mapi_namespace + node
+            try:
+                res = self._mapi.lookupNode(name)
+                if not isinstance(res, str): raise Exception('')
+            except:
+                self._mapi_last_result = False
+                break
+
+        return self._mapi_last_result
+
+    def _chk_ground_heartbeat(self):
+        ''' Check if ground heartbeat has been received recently '''
+        if self._hb_last_msg is None:
+            return False
+
+        t = rospy.get_time()
+        if t > self._hb_last_time + self._hb_interval:
+            return False
+
+        return True
+
+    def _chk_autopilot_pose(self):
+        ''' Check if autopilot pose has been received recently '''
+        if self._ap_last_msg is None:
+            return False
+
+        t = rospy.get_time()
+        if t > self._ap_last_time + self._ap_interval:
+            return False
+
+        return True
 
 
 #-----------------------------------------------------------------------
@@ -64,30 +187,16 @@ def next_counter():
 
 if __name__ == '__main__':
     # ROS initialization
-    rospy.init_node('ap_safety_monitor')
+    rospy.init_node('safety')
     
-    # Set up ROS publishers
-    pub_heartbeat = rospy.Publisher("%s/heartbeat"%ROS_BASENAME,
-                                    apmsg.Heartbeat, queue_size=1)
+    # Set up monitor object
+    monitor = SafetyMonitor()
     
-    # Set up ROS subscribers
-    
-    
-    # Set up ROS services
-    rospy.Service("%s/set_health_state"%ROS_BASENAME,
-                  ap_srvs.SetBoolean, svc_set_health)
-    
-    # Start main loop
-    hb = apmsg.Heartbeat()
-    r = rospy.Rate(1.0)
+    # Wrap monitor run loop in try block, never let it die
     print "\nStarting safety monitor loop...\n"
     while not rospy.is_shutdown():
-        # Publish a heartbeat, if all is well
-        if all_is_well:
-            hb.counter = next_counter()
-            pub_heartbeat.publish(hb)
-        
-        # Sleep so ROS subscribers and services can run
-        r.sleep()
-
+        try:
+            monitor.run()
+        except Exception as ex:
+            rospy.logwarn("SAFETY: loop error: " + str(ex))
 
